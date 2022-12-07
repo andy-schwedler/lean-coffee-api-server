79361039da3a336b7cb2bb537d996fc5d88bb0b7
Andy Schwedler <116001730+andy-schwedler@users.noreply.github.com>
2022-12-07 13:08:31 +0100

    add post method

--- pages/api/questions/index.js
+++ pages/api/questions/index.js
@@ -2,20 +2,31 @@ import dbConnect from "../../../db/dbConnect";
 import Question from "../../../db/models/Question";
 
 export default async function handler(req, res) {
-  //1. Connect to the database and wait for the connection to be established
   await dbConnect();
 
-  // 2. Read data from database (async! use await!)
-  const questions = await Question.find(); // this is a mongoose collection
+  if (req.method === "GET") {
+    const questions = await Question.find(); // this is a mongoose collection
 
-  // 3. Translate collection to array
-  const questionsArray = questions.map((question) => {
-    return {
-      id: question._id,
-      name: question.name,
-      text: question.text,
-    };
-  });
+    const questionsArray = questions.map((question) => {
+      return {
+        id: question._id,
+        name: question.name,
+        text: question.text,
+      };
+    });
 
-  res.status(200).json(questionsArray);
+    res.status(200).json(questionsArray);
+  }
+
+  if (req.method === "POST") {
+    const data = req.body;
+    try {
+      const newQuestion = await Question.create(data);
+
+      // 201 => created
+      return res.status(201).json(newQuestion);
+    } catch (error) {
+      res.status(400).json({ message: "error" });
+    }
+  }
 }
